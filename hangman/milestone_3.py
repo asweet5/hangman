4a9199b5684fc0ce6f9a46fe1017cc0100318985
asweet5 <alexanderjsweet@gmail.com>
2023-09-25 21:10:28 +0100

    Creates functions for asking for input and checking if guess is correct

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -3,16 +3,22 @@ import random
 word_list = ['apple', 'strawberry', 'banana', 'blackberry', 'raspberry']
 word = random.choice(word_list)
 print(word)
+    
+def ask_for_input():
+    while True:
+        guess = input('Enter a sinlge letter as your guess: ')
+        if len(guess) == 1 and guess.isalpha() == True:
+            print('Good guess')
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(guess)
 
-while True:
-    guess = input('Enter a sinlge letter as your guess: ')
-    if len(guess) == 1 and guess.isalpha() == True:
-        print('Good guess')
-        break
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
     else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
+        print(f'Sorry {guess} is not in the word try again.')
 
-if guess in word:
-    print(f'Good guess! {guess} is in the word.')
-else:
-    print(f'Sorry {guess} is not in the word try again.')
+ask_for_input()

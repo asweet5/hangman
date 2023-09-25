723b22f2f7f193e95445bcf25abdb83bbb67b37e
asweet5 <alexanderjsweet@gmail.com>
2023-09-25 21:04:45 +0100

    Checks if user input is in randomly chosen word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,3 +1,9 @@
+import random
+
+word_list = ['apple', 'strawberry', 'banana', 'blackberry', 'raspberry']
+word = random.choice(word_list)
+print(word)
+
 while True:
     guess = input('Enter a sinlge letter as your guess: ')
     if len(guess) == 1 and guess.isalpha() == True:
@@ -5,3 +11,8 @@ while True:
         break
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
+
+if guess in word:
+    print(f'Good guess! {guess} is in the word.')
+else:
+    print(f'Sorry {guess} is not in the word try again.')
